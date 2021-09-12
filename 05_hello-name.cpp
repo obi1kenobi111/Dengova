e78009e8b022dbfde9a67d953d4854b32570c348
obi1kenobi111 <58305221+obi1kenobi111@users.noreply.github.com>
2021-09-12 20:46:07 +0300

    Add files via upload

---
+++ 05_hello-name.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <stdio.h> 
+#include <string> // для работы со строками
+int main()
+{
+    //Чтобы в консоль выводить слова на рус:
+    system("chcp 1251");
+    system("cls");
+    // setlocale(LC_ALL, "Russian");
+
+
+//ЗАДАНИЕ 6
+    char k[15];
+    std::cout<<"Введите ваше имя:";
+    gets_s(k);
+    std::cout <<"Привет,"<<k<<"!";
+
+    gets_s(k); //получаем строку на вводе
+
+    
+}
---
+++ 06_StrokaCopy.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <stdio.h> 
+#include <string> // для работы со строками
+int main()
+{
+    //Чтобы в консоль выводить слова на рус:
+    system("chcp 1251");
+    system("cls");
+    // setlocale(LC_ALL, "Russian");
+
+
+//ЗАДАНИЕ 6
+    char k[15];
+    printf("Введите строку: ");
+
+
+    gets_s(k); //получаем строку на вводе
+
+    int count = strlen(k);
+    printf("Копия строки:  ");
+    for (int i = 0; i <= count; i++)
+    {
+        std::cout << k[i];
+    }
+
+    printf("\n");
+
+}
